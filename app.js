f87c3060f83de96b1686c7931b91414953386ab4
asad <asad@MDs-MacBook-Pro.lan>
2020-09-20 13:58:32 -0500

    Adding CORS support

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
