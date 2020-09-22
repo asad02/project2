9c3abb6cf69ba14c14602bc42317484b7f7d3cad
asad <asad@MDs-MacBook-Pro.lan>
2020-09-22 18:09:34 -0500

    working on orders

--- api/routes/products.js
+++ api/routes/products.js
@@ -40,7 +40,6 @@ router.post('/', (req, res, next) => {
     product
         .save()
         .then(result => {
-            console.log(result);
             res.status(201).json({
                 message: 'Created product successfully',
                 createdProduct: {
